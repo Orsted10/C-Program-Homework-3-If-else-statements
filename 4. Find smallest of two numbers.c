7cba5b0cf521bbf273097dc130af076e03e036d3
Ankan Bhattacharjee <113756091+Orsted10@users.noreply.github.com>
2025-08-31 10:47:34 +0530

    Create 4. Find smallest of two numbers.c

---
+++ 4. Find smallest of two numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    
+    if (a < b) {
+        printf("%d is the smallest\n", a);
+    } else {
+        printf("%d is the smallest\n", b);
+    }
+    
+    return 0;
+}
